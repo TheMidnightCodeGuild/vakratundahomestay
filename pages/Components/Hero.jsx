e5d5b91c390ffd0c1e3ce8b4a6fe30b645731448
mayanktilwankar2355@gmail.com <mayanktilwankar2355@gmail.com>
2024-11-05 13:41:19 +0530

    don

--- pages/Components/Hero.jsx
+++ pages/Components/Hero.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div
-      className="relative h-screen w-full bg-cover bg-center bg-no-repeat"
+      className="relative sm:h-screen h-[95vh] w-full bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: "url('/images/lal2.png')" }}>
       <div className="absolute inset-0 bg-black opacity-60"></div>
       <div className="relative z-10 flex flex-col justify-center items-center h-full text-center text-white px-4 sm:px-6 lg:px-8">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl font-bold mb-2 sm:mb-4 tracking-tight leading-tight max-w-4xl">
+        <h1 className="text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl font-bold mb-2 sm:mb-4 tracking-tight leading-tight max-w-4xl">
           Discover Vakratunda Homestay in Ujjain
         </h1>
-        <h2 className="text-lg sm:text-xl md:text-2xl lg:text-2xl mb-4 sm:mb-8 font-light italic">
+        <h2 className="text-base sm:text-xl md:text-2xl lg:text-2xl mb-4 sm:mb-8 font-light italic">
           Where Comfort Meets Spirituality
         </h2>
-        <p className="mb-6 sm:mb-10 text-base sm:text-lg md:text-lg max-w-xs sm:max-w-lg md:max-w-2xl lg:max-w-3xl leading-relaxed">
+        <p className="mb-6  sm:mb-10 text-base sm:text-lg md:text-lg max-w-xs sm:max-w-lg md:max-w-2xl lg:max-w-3xl leading-relaxed">
           &quot;Immerse yourself in the rich culture of Ujjain while enjoying
           modern amenities. Our homestay offers a perfect blend of traditional
           charm and contemporary comfort, just steps away from the sacred
